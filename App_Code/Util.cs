94f2774176895ed384f568758d552248cf7cf17d
cangjie <cangjie@hotmail.com>
2015-03-18 15:31:36 +0800

    add a api for token

--- App_Code/Util.cs
+++ App_Code/Util.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.Text;
+using System.Security.Cryptography;
 /// <summary>
 /// Summary description for Util
 /// </summary>
@@ -18,6 +19,40 @@ public class Util
 		// TODO: Add constructor logic here
 		//
 	}
+
+    public static string GetSHA1(string str)
+    {
+        SHA1 sha = SHA1.Create();
+        ASCIIEncoding enc = new ASCIIEncoding();
+        byte[] bArr = enc.GetBytes(str);
+        bArr = sha.ComputeHash(bArr);
+        string validResult = "";
+        for (int i = 0; i < bArr.Length; i++)
+        {
+            validResult = validResult + bArr[i].ToString("x").PadLeft(2, '0');
+        }
+        return validResult.Trim();
+    }
+
+    public static string GetMd5(string str)
+    {
+        System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
+        byte[] bArr = md5.ComputeHash(Encoding.UTF8.GetBytes(str));
+        string ret = "";
+        foreach (byte b in bArr)
+        {
+            ret = ret + b.ToString("x").PadLeft(2, '0');
+        }
+        return ret;
+    }
+
+    public static string GetLongTimeStamp(DateTime currentDateTime)
+    {
+        TimeSpan ts = currentDateTime - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+        return Convert.ToInt64(ts.TotalMilliseconds).ToString();
+        //TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+        //return Convert.ToInt64(ts.TotalSeconds).ToString();
+    }
 
     
 }
